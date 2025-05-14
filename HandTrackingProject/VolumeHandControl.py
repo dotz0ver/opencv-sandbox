f78532837ce285d9c04b5325e9e26e8755071545
dotz0ver <opir0oui@gmail.com>
2025-05-14 18:53:56 +0900

    Add gesture-based volume control using thumb and index finger distance

--- HandTrackingProject/VolumeHandControl.py
+++ HandTrackingProject/VolumeHandControl.py
@@ -2,6 +2,9 @@ import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
+import math
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 wCam, hCam = 640, 480
 
@@ -12,18 +15,47 @@ pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7)
 
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange() # (-65.25, 0.0, 0.03125) -> (최소 볼륨, 최대 볼륨, 볼륨 조절 최소 단위 간격)
+volume.SetMasterVolumeLevel(-20.0, None)
+minVol = volRange[0]
+maxVol = volRange[1]
+
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
-        print(lmList[4], lmList[8]) # 엄지, 검지의 끝(tip) 좌표
+        # print(lmList[4], lmList[8]) # 엄지, 검지의 끝(tip) 좌표
 
-        x1, y1 = lmList[4][1], lmList[4][2]
-        x2, y2 = lmList[8][1], lmList[8][2]
+        x1, y1 = lmList[4][1], lmList[4][2] # 엄지
+        x2, y2 = lmList[8][1], lmList[8][2] # 검지
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
         cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3) # 엄지와 검지를 잇는 라인
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED) # 위 라인의 중간 지점 circle 생성
+
+        length = math.hypot(x2-x1, y2-y1)
+        print(length) # 이걸로 min:20, max:300 알 수 있음
+
+        # Hand range 20 - 300
+        # Volume Range -65 - 0
+
+        vol = np.interp(length, [20, 300], [minVol, maxVol])
+        print(vol)
+
+        if length<50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
