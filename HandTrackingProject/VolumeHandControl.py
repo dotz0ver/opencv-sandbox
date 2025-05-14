cd92cd15ccd069c9216c4adbe9f06ecb47cdd658
dotz0ver <opir0oui@gmail.com>
2025-05-14 17:59:20 +0900

    Visualize thumb and index finger tips with circles

--- HandTrackingProject/VolumeHandControl.py
+++ HandTrackingProject/VolumeHandControl.py
@@ -10,11 +10,20 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
-detector = htm.handDetector()
+detector = htm.handDetector(detectionCon=0.7)
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        print(lmList[4], lmList[8]) # 엄지, 검지의 끝(tip) 좌표
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
