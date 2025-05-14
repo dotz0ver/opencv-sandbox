1fea35111c57270d819bc555380e3c3e81ba15a4
dotz0ver <opir0oui@gmail.com>
2025-05-14 14:54:35 +0900

    Add basic webcam streaming with FPS overlay

--- HandTrackingProject/VolumeHandControl.py
+++ HandTrackingProject/VolumeHandControl.py
@@ -2,10 +2,20 @@ import cv2
 import time
 import numpy as np
 
-cap = cv2.VideoCapture(1)
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
 
 while True:
     success, img = cap.read()
 
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,f'FPS: {int(fps)}',(40,70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
     cv2.imshow("Img", img)
     cv2.waitKey(1)
