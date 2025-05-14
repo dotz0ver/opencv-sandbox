89f451af8b18ff9ec30752f7c86d85d071cb9e85
dotz0ver <opir0oui@gmail.com>
2025-05-14 19:17:01 +0900

    Visualize volume level with bar and percentage based on finger distance

--- HandTrackingProject/VolumeHandControl.py
+++ HandTrackingProject/VolumeHandControl.py
@@ -25,6 +25,9 @@ volume = interface.QueryInterface(IAudioEndpointVolume)
 volRange = volume.GetVolumeRange() # (-65.25, 0.0, 0.03125) -> (최소 볼륨, 최대 볼륨, 볼륨 조절 최소 단위 간격)
 minVol = volRange[0]
 maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
 
 
 while True:
@@ -50,12 +53,17 @@ while True:
         # Volume Range -65 - 0
 
         vol = np.interp(length, [20, 300], [minVol, maxVol])
+        volBar = np.interp(length, [20, 300], [400, 150])
+        volPer = np.interp(length, [20, 300], [0, 100])
         print(int(length), vol)
         volume.SetMasterVolumeLevel(vol, None)
 
         if length<50:
             cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
 
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f': {int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
