ab6568c0753a740b2c87e2a37e0525a79e233e5a
dotz0ver <opir0oui@gmail.com>
2025-05-14 17:45:10 +0900

    Integrate hand tracking module with webcam stream

--- HandTrackingProject/VolumeHandControl.py
+++ HandTrackingProject/VolumeHandControl.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import numpy as np
+import HandTrackingModule as htm
 
 wCam, hCam = 640, 480
 
@@ -9,13 +10,16 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
+detector = htm.handDetector()
+
 while True:
     success, img = cap.read()
+    img = detector.findHands(img)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
 
-    cv2.putText(img,f'FPS: {int(fps)}',(40,70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
+    cv2.putText(img,f'FPS: {int(fps)}',(40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
     cv2.imshow("Img", img)
     cv2.waitKey(1)
